8ea7f8f933a3c7d5eb1ec377ef3b0fbbe06ede8f
Laura Nunes <lauranunest@hotmail.com>
2025-09-28 19:46:33 -0300

    fix: db

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
 var host = Environment.GetEnvironmentVariable("DB_HOST");
 var port = Environment.GetEnvironmentVariable("DB_PORT");
 var database = Environment.GetEnvironmentVariable("DB_NAME");
@@ -11,15 +15,11 @@ var password = Environment.GetEnvironmentVariable("DB_PASS");
 
 var connectionString = $"Host={host};Port={port};Database={database};Username={user};Password={password}";
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString, o =>
     {
-        o.CommandTimeout(60);
-        o.EnableRetryOnFailure(5);
+        o.CommandTimeout(60);    
+        o.EnableRetryOnFailure(5); 
     })
 );
 
@@ -33,9 +33,15 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+app.UseHttpsRedirection();
 app.UseCors("AllowAll");
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
@@ -43,7 +49,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
