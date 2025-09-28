4895632666f158ea0cda35d5d7d81f0f5efc8ad5
Laura Nunes <lauranunest@hotmail.com>
2025-09-28 19:17:00 -0300

    fix: timeout db

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,23 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Pega as variáveis de ambiente do Render
 var host = Environment.GetEnvironmentVariable("DB_HOST");
 var port = Environment.GetEnvironmentVariable("DB_PORT");
 var database = Environment.GetEnvironmentVariable("DB_NAME");
 var user = Environment.GetEnvironmentVariable("DB_USER");
 var password = Environment.GetEnvironmentVariable("DB_PASS");
 
-// Monta a connection string real
 var connectionString = $"Host={host};Port={port};Database={database};Username={user};Password={password}";
-
-// Configura o DbContext com a connection string real
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString, o =>
+    {
+        o.CommandTimeout(60);
+        o.EnableRetryOnFailure(5);
+    })
+);
 
 builder.Services.AddCors(options =>
 {
